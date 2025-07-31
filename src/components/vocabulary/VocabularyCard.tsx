5921e22ef17979d8a8c1e9685903a4c16e52d2f8
catisverycute <105216377+catisverycute@users.noreply.github.com>
2025-07-31 23:43:06 +0900

    ✨ [new features] 단어장 컴포넌트 생성

---
+++ src/components/vocabulary/VocabularyCard.tsx
@@ -0,0 +1,72 @@
+import { useState, type JSX } from 'react';
+import DeleteConfirmModal from './DeleteCofirmModal';
+import { useColorStore } from '../../stores/colorStore';
+
+interface VocabularyCardProps {
+  title: string;
+}
+
+export default function VocabularyCard({
+  title,
+}: VocabularyCardProps): JSX.Element {
+  const selectedColor = useColorStore((state) => state.selectedColor);
+  const [showDeleteModal, setShowDeleteModal] = useState(false);
+  const [showPinButton, setShowPinButton] = useState(false);
+  const [showDeleteButton, setShowDeleteButton] = useState(false);
+  const [touchX, setTouchX] = useState(0);
+  const [translateX, setTranslateX] = useState(0);
+  const handleTouchStart = (e: React.TouchEvent<HTMLDivElement>) => {
+    setTouchX(e.touches[0].clientX);
+  };
+
+  const handleTouchMove = (e: React.TouchEvent<HTMLDivElement>) => {
+    const deltaX = e.touches[0].clientX - touchX;
+    setTranslateX(deltaX);
+  };
+
+  const handleTouchEnd = () => {
+    if (translateX < -50) {
+      setShowDeleteButton(true);
+      setShowPinButton(false);
+    } else if (translateX > 50) {
+      setShowDeleteButton(false);
+      setShowPinButton(true);
+    } else {
+      setShowDeleteButton(false);
+      setShowPinButton(false);
+    }
+    setTranslateX(0);
+  };
+  return (
+    <div className="flex flex-col justify-center my-8">
+      <div
+        className={`flex justify-between ${selectedColor} w-72 rounded-lg`}
+        onTouchStart={handleTouchStart}
+        onTouchMove={handleTouchMove}
+        onTouchEnd={handleTouchEnd}
+      >
+        <button
+          className={`bg-blue-300 w-20 rounded-l-lg ${showPinButton ? 'block' : 'hidden'}`}
+        >
+          pin
+        </button>
+        <div className="p-6 w-full">{title}</div>
+        <button
+          className={`bg-red-400  w-20 rounded-r-lg ${showDeleteButton ? 'opacity-100' : 'opacity-0'}`}
+          onClick={() => {
+            console.log('삭제 클릭');
+            setShowDeleteModal(true);
+          }}
+        >
+          삭제
+        </button>
+        {showDeleteModal && (
+          <DeleteConfirmModal
+            onCancel={() => setShowDeleteModal(false)}
+            vocabularyName={title}
+          />
+        )}
+      </div>
+    </div>
+  );
+}
