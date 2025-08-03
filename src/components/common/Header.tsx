01965726975318acbc334a9889a80a2663737f94
catisverycute <105216377+catisverycute@users.noreply.github.com>
2025-08-04 02:19:12 +0900

    ♻️ [refactor] 헤더 타이틀을 클릭 시 홈으로 이동하도록 수정

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,9 +1,12 @@
 import type { JSX } from 'react';
+import { Link } from 'react-router-dom';
 
 export default function Header(): JSX.Element {
   return (
     <div className="flex justify-between">
-      <h2>Memon</h2>
+      <Link to={'/'}>
+        <h2>Memon</h2>
+      </Link>
       <div className="flex gap-2">
         <div>dark mode</div>
         <div>market</div>
