6af4732c54bdfd5c8c46ac5ee98c3e32fe551a6d
catisverycute <105216377+catisverycute@users.noreply.github.com>
2025-08-04 02:12:50 +0900

    🔥 [remove] 불필요한 라우트 제거

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -1,7 +1,6 @@
 import { Route, Routes } from 'react-router-dom';
 import HomePage from '../page/HomePage';
 import MainLayout from '../layout/MainLayout';
-import VocabularyPage from '../page/VocabularyPage';
 import LearnPage from '../page/LearnPage';
 
 export default function Router() {
@@ -9,7 +8,6 @@ export default function Router() {
     <Routes>
       <Route element={<MainLayout />}>
         <Route path="/" element={<HomePage />} />
-        <Route path="/vocabulary" element={<VocabularyPage />} />
         <Route path="/learn/:id" element={<LearnPage />} />
       </Route>
     </Routes>
