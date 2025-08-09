4f2ef5b787371eb5787cd9c3371487a6a5431188
catisverycute <105216377+catisverycute@users.noreply.github.com>
2025-08-09 23:27:04 +0900

    ✨ [new features] AddWordButton에 props 추가 및 onClick 이벤트 연결

--- src/components/word-list/AddWordButton.tsx
+++ src/components/word-list/AddWordButton.tsx
@@ -1,9 +1,16 @@
 import { PlusCircle } from 'lucide-react';
 
-export default function AddWordButton() {
+interface AddButtonProps {
+  addWord: () => void;
+}
+
+export default function AddWordButton({ addWord }: AddButtonProps) {
   return (
     <div className="flex justify-center">
-      <button className="px-10 py-3 rounded flex font-semibold gap-2 bg-primary">
+      <button
+        className="px-10 py-3 rounded flex font-semibold gap-2 bg-primary"
+        onClick={addWord}
+      >
         단어 추가 <PlusCircle />
       </button>
     </div>
