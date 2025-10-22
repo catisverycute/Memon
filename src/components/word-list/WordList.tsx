a23a6e8b219aea77db1880c2facb755ec72950eb
catisverycute <105216377+catisverycute@users.noreply.github.com>
2025-10-23 01:15:36 +0900

    ✨ [new features] 단어 리스트에 즐겨찾기 버튼 및 클릭 이벤트 추가

--- src/components/word-list/WordList.tsx
+++ src/components/word-list/WordList.tsx
@@ -1,16 +1,19 @@
-import type { JSX } from 'react';
+import { type JSX } from 'react';
+import FavoriteButton from './FavoriteButton';
+import { getBook } from '../../api/bookApi';
+// import { fetchUsers } from '../../api/wordApi';
 
 export default function WordList(): JSX.Element {
+  const handleFetch = () => {
+    getBook();
+  };
   return (
     <div className="my-5">
       <div>150 글자</div>
-      <div className="flex gap-2 border p-2">
-        <div> border</div>
-        <div>n. 경계</div>
-      </div>
-      <div className="border my-2 p-2">
-        <div> border</div>
-        <div>n. 경계</div>
+      <div className="flex justify-between gap-2 border p-4 rounded-md">
+        <div>border</div>
+        <div onClick={handleFetch}>n. 경계</div>
+        <FavoriteButton />
       </div>
     </div>
   );
