e64aa93710faa1307e9c6fe5a0ad7413afd3333d
catisverycute <ivyseo0312@gmail.com>
2025-11-20 16:23:08 +0900

    ✨ [new features] 단어장 페이지 이동 방식 navigate(state)로 변경

--- src/components/word-list/WordList.tsx
+++ src/components/word-list/WordList.tsx
@@ -2,17 +2,23 @@ import { type JSX } from 'react';
 import FavoriteButton from './FavoriteButton';
 import { getBook } from '../../api/bookApi';
 // import { fetchUsers } from '../../api/wordApi';
+interface WordListProps {
+  word: string;
+  meaning: string;
+}
 
-export default function WordList(): JSX.Element {
+export default function WordList({
+  word,
+  meaning,
+}: WordListProps): JSX.Element {
   const handleFetch = () => {
     getBook();
   };
   return (
     <div className="my-5">
-      <div>150 글자</div>
       <div className="flex justify-between gap-2 border p-4 rounded-md">
-        <div>border</div>
-        <div onClick={handleFetch}>n. 경계</div>
+        <div>{word}</div>
+        <div onClick={handleFetch}>{meaning}</div>
         <FavoriteButton />
       </div>
     </div>
