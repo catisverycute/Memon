bdf9c6d53f5f8bdfc7c9cbe22acda3542fe0cf58
catisverycute <ivyseo0312@gmail.com>
2025-11-20 16:22:53 +0900

    ♻️ [refactor] VocabularyListPage에서 list 제목/단어 렌더링 구조 개선

--- src/page/VocabularyListPage.tsx
+++ src/page/VocabularyListPage.tsx
@@ -4,14 +4,23 @@ import WordListHeader from '../components/word-list/WordListHeader';
 import SearchWord from '../components/word-list/SearchWord';
 import AddWordButton from '../components/word-list/AddWordButton';
 import CreateWordModal from '../components/word-list/CreateWordModal';
+import { toeicWords } from '../data/mockWords';
+import { useLocation } from 'react-router-dom';
 
 export default function VocabularyListPage(): JSX.Element {
   const [openModal, setOpenModal] = useState(false);
+  const location = useLocation();
+  const state = location.state;
+  const title = state.title;
+
   return (
     <div>
-      <WordListHeader listName="HI" />
+      <WordListHeader listName={title} />
       <SearchWord />
-      <WordList />
+      <div className="my-4">총 단어 수 : {toeicWords.length}개</div>
+      {toeicWords.map((words) => (
+        <WordList word={words.word} meaning={words.meaning} />
+      ))}
       <AddWordButton addWord={() => setOpenModal(true)} />
       {openModal && <CreateWordModal />}
     </div>
