413648e6ca329fc76ddf29e63347acf6e189905c
catisverycute <ivyseo0312@gmail.com>
2025-11-20 16:12:38 +0900

    🧹 [chore] 샘플 단어장 데이터 추가

--- src/data/mockVocabulary.ts
+++ src/data/mockVocabulary.ts
@@ -1,22 +1,59 @@
+import type { Vocabulary } from '../types/vocabulary';
 import { toeicWords } from './mockWords';
 
-export const mockVocabulary = [
+export const mockVocabulary: Vocabulary[] = [
   {
     id: 1,
     title: 'TOEIC 단어장',
-    color: 'bg-dol',
+    color: 'bg-pastelLavenderLight',
     words: toeicWords,
   },
   {
     id: 2,
     title: 'JLPT N3 단어장',
-    color: 'bg-pastelRose',
+    color: 'bg-pastelLemon',
     words: [],
   },
   {
     id: 3,
     title: '영어 5과',
-    color: 'bg-lime',
+    color: 'bg-pastelGreen',
+    words: [],
+  },
+  {
+    id: 4,
+    title: '영단어',
+    color: 'bg-pastelSkyLight',
+    words: [],
+  },
+  {
+    id: 5,
+    title: '중국어',
+    color: 'bg-pastelSkyLight',
+    words: [],
+  },
+  {
+    id: 6,
+    title: '영단어',
+    color: 'bg-pastelSkyLight',
+    words: [],
+  },
+  {
+    id: 7,
+    title: '영단어',
+    color: 'bg-pastelSkyLight',
+    words: [],
+  },
+  {
+    id: 8,
+    title: '영단어',
+    color: 'bg-pastelSkyLight',
+    words: [],
+  },
+  {
+    id: 9,
+    title: '영단어',
+    color: 'bg-pastelSkyLight',
     words: [],
   },
 ];
